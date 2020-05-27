5c32e62074d29ce8fbb5c75b25c0718d37b97964
Michael Luxton <lux@Michaels-iMac.hsd1.ca.comcast.net>
2020-05-27 15:16:28 -0700

    improve date slider

--- comap/when.js
+++ comap/when.js
@@ -1,11 +1,11 @@
 
 class When {
 	update() {
-		var oldest = Date.parse("1/22/20");
+		var oldest = Date.parse("1/23/20");
 		var newest = Date.now();
 		var when = document.getElementById("when").value;
 		var span = newest - oldest;
-		var cutoff = oldest + Math.trunc(when * span / 100);
+		var cutoff = oldest + Math.trunc(when * span / 1000);
 		var date = new Date();
 		date.setTime(cutoff);
 		var dateStr = date.getFullYear() + "-" + (date.getMonth() + 1).toString() + "-" + date.getDate();
