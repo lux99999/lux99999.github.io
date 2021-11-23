488384e5c4f8fc3c6df3defc4cb074cfcfb27718
Michael Luxton <lux@Michaels-iMac.hsd1.ca.comcast.net>
2021-11-23 09:21:11 -0800

    example

--- misc/Circle.java
+++ misc/Circle.java
@@ -8,14 +8,14 @@ public class Circle {
     public Circle(int width, String color) {        // Constructor. Some languages actually call this "constructor" but in Java, you just define a method whose name matches the class's name.
         this.width = width;                         // "this" means the instance that is being constructed.
         this.color = color;                         // The constructor's job is to install values into the instance...
-        Circle.count ++;                            // ...and to update the count of circles.
+        Circle.count ++;                            // ...and to update the overall count of circles.
     }
     
     public void print() {                           // Normal method. You call this using an instance.
         System.out.println("I am a circle, " + this.width + " and " + this.color + "!");
     }
     
-    public static void printCount() {               // Static method. You call this using the class itself, and it can only refer to static attributes.
+    public static void printCount() {               // Static method. You call this using the Circle class itself, and it can only refer to static attributes.
         System.out.println("There are " + Circle.count + " circles.");
     }
     
