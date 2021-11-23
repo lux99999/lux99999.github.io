086a06ed9427dd91074277fd5b4ef37dbc90c937
Michael Luxton <lux@Michaels-iMac.hsd1.ca.comcast.net>
2021-11-23 09:25:05 -0800

    example

--- misc/Circle.java
+++ misc/Circle.java
@@ -1,9 +1,9 @@
 public class Circle {
     
-    public static int count;                        // Static attribute. The Circle class itself has the only copy. 
+    public static int count;                        // Static field. The Circle class itself has the only copy. 
     
-    public int width;                               // Normal attribute. Each instance has its own copy.
-    public String color;                            // Normal attribute. Each instance has its own copy.
+    public int width;                               // Normal field. Each instance has its own copy.
+    public String color;                            // Normal field. Each instance has its own copy.
     
     public Circle(int width, String color) {        // Constructor. Some languages actually call this "constructor" but in Java, you just define a method whose name matches the class's name.
         this.width = width;                         // "this" means the instance that is being constructed.
@@ -15,7 +15,7 @@ public class Circle {
         System.out.println("I am a circle, " + this.width + " and " + this.color + "!");
     }
     
-    public static void printCount() {               // Static method. You call this using the Circle class itself, and it can only refer to static attributes.
+    public static void printCount() {               // Static method. You call this using the Circle class itself, and it can only refer to static fields.
         System.out.println("There are " + Circle.count + " circles.");
     }
     
